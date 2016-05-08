0cc959d1fec2f7627bd2b59b5b504af67553eff3
Eduardo Alvarez <walojose46@gmail.com>
2016-05-07 21:00:27 -0400

    agrega al arbol los repeat

--- src/ast/NodoFor.java
+++ src/ast/NodoFor.java
@@ -2,41 +2,30 @@ package ast;
 
 public class NodoFor extends NodoBase{
 	
-	private NodoBase variable;
-	private NodoBase valorInicial;
+	private NodoBase asignacion;
 	private NodoBase valorFinal;
 	private NodoBase cuerpo;
 	
 	public NodoFor(){
 		super();
-		this.variable = null;
-		this.valorInicial = null;
+		this.asignacion = null;
 		this.valorFinal = null;
 		this.cuerpo = null;
 	}
 	
-	public NodoFor(NodoBase variable, NodoBase valorInicial, NodoBase valorFinal, NodoBase cuerpo){
+	public NodoFor(NodoBase asignacion, NodoBase valorFinal, NodoBase cuerpo){
 		super();
-		this.variable = variable;
-		this.valorInicial = valorInicial;
+		this.asignacion = asignacion;
 		this.valorFinal = valorFinal;
 		this.cuerpo = cuerpo;
 	}
 
 	public NodoBase getVariable() {
-		return variable;
+		return asignacion;
 	}
 
-	public void setVariable(NodoBase variable) {
-		this.variable = variable;
-	}
-
-	public NodoBase getValorInicial() {
-		return valorInicial;
-	}
-
-	public void setValorInicial(NodoBase valorInicial) {
-		this.valorInicial = valorInicial;
+	public void setVariable(NodoBase asignacion) {
+		this.asignacion = asignacion;
 	}
 
 	public NodoBase getValorFinal() {
